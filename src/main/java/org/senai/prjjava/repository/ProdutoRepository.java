c411b9829d9d3d36b781282b73c74c1b0e8d1da2
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-11-13 19:30:41 -0300

    Adicionando API de Produtos

---
+++ src/main/java/org/senai/prjjava/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package org.senai.prjjava.repository;
+import org.springframework.data.repository.CrudRepository;
+import org.senai.prjjava.entity.Produto;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+
+}
