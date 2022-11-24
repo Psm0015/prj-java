240c662046175b24c9f247556d9c6a42c82cdc4d
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-11-24 20:42:18 -0300

    Recriando Metodo Delete e buscar usuario

--- src/main/java/org/senai/prjjava/controller/UsuarioController.java
+++ src/main/java/org/senai/prjjava/controller/UsuarioController.java
@@ -9,39 +9,42 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
-    private final UsuarioRepository repository;
+    private final UsuarioRepository urepository;
     private final PasswordEncoder encoder;
 
     public UsuarioController(UsuarioRepository repository, PasswordEncoder encoder){
-        this.repository = repository;
+        this.urepository = repository;
         this.encoder = encoder;
     }
 
     @GetMapping("/")
     public ResponseEntity<List<Usuario>> listarTodos(){
-        return ResponseEntity.ok(repository.findAll());
+        return ResponseEntity.ok(urepository.findAll());
     }
 
     @PostMapping("/")
     public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
         usuario.setPassword(encoder.encode(usuario.getPassword()));
-        return ResponseEntity.ok(repository.save(usuario));
+        return ResponseEntity.ok(urepository.save(usuario));
     }
     @GetMapping("/login")
     public ResponseEntity<Boolean> validarSenha(@RequestParam String login,@RequestParam String password){
 
-        Optional<Usuario> optUsuario = repository.findByLogin(login);
+        Optional<Usuario> optUsuario = urepository.findByLogin(login);
         if (optUsuario.isEmpty()){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
         }
@@ -51,5 +54,14 @@ public class UsuarioController {
         HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
 
         return ResponseEntity.status(status).body(valid);
+    }
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Usuario> buscarUsuario(@PathVariable Integer id){
+        return urepository.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    public @ResponseBody String apagar(@PathVariable Integer id){
+        urepository.deleteById(id);
+        return "Usuario deletado com Sucesso!";
     }
 }
