92daf721df93447e0882edaa71bad006c0555004
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-11-26 14:39:43 -0300

    cross origin e mudando de conexão

--- src/main/java/org/senai/prjjava/controller/UsuarioController.java
+++ src/main/java/org/senai/prjjava/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuario")
+@CrossOrigin("*")
 public class UsuarioController {
 
     private final UsuarioRepository urepository;
