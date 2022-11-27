f90002538053a257f08620a04923c20a544d0a98
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-11-27 18:23:03 -0300

    Corrigindo Problema no Cors

--- src/main/java/org/senai/prjjava/security/JWTConfiguracao.java
+++ src/main/java/org/senai/prjjava/security/JWTConfiguracao.java
@@ -31,7 +31,7 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
