3fa69493c7c2f7c5eb21d4f4e8b19e9f0cf8f834
pedro-mota <pedro.mota@df.estudante.senai.br>
2022-12-22 20:20:03 -0300

    Compra via whatsapp

--- src/main/resources/static/js/carrinho.js
+++ src/main/resources/static/js/carrinho.js
@@ -7,6 +7,7 @@ function vercarrinho(){
         obj = JSON.parse(sessionStorage.getItem('cart'))
         txt=''
         total =0.0
+        produtos = ''
         for(var i in obj){
             console.log(obj[i])
             const ajax = new XMLHttpRequest();
@@ -15,17 +16,14 @@ function vercarrinho(){
             ajax.setRequestHeader("Authorization", sessionStorage.getItem('token'));
             ajax.send();
             ajax.onload = function(){
-                produtos += prd.nome + '%0A'
-                x = sessionStorage.getItem('prds')
-                x += produtos
-                x = sessionStorage.setItem('prds', produtos)
                 prd = JSON.parse(this.responseText)
+                produtos += prd.nome + ', '
                 txt += "<tr><td>"+prd.nome+"</td>"
                 txt += "<td>"+prd.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })+" </td>"
                 txt += "<td><input type='button' value='Apagar' onclick='apagarprd("+i+")' class='btn btn-danger'></td></tr>"
                 total += prd.valor
-                document.getElementById('valorpagar').value = 'Pagar '+total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-                sessionStorage.setItem('tot',total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+                msg = "'Olá gostaria de compra ("+produtos+") Com valor total de: "+total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+                document.getElementById('btnpg').innerHTML = '<input id="valorpagar" onclick="wtts('+(msg+="'")+')" type="button" value="Pagar '+total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })+'" class="btn btn-success">'
                 console.log(total)
                 document.getElementById('listacarrinho').innerHTML += txt
                 txt = ''
@@ -45,6 +43,14 @@ function pagar(){
     document.getElementById('valorpagar').value
     total = sessionStorage.getItem('tot')
     x = sessionStorage.getItem('prds')
-    // window.location.href = 'https://wa.me/5561999766526?text=Olá%20tudo%20bem,%20Gostaria%20de%20comprar:%0A'
+    window.location.href = 
     console.log(total , x)
+}
+function wtts(msg){
+    let number = '5561999766526'
+    // o texto ou algo vindo de um <textarea> ou <input> por exemplo
+    // montar o link (número e texto) (web)
+    target = `https://api.whatsapp.com/send?phone=${encodeURIComponent(number)}&text=${encodeURIComponent(msg)}`
+    window.location.href = target;
+    return target
 }
