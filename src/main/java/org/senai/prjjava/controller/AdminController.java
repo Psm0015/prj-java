ca404e72ad601062a3267fea8dd90b6f991042cc
pedro-mota <pedro.mota@df.estudante.senai.br>
2023-01-23 21:44:20 -0300

    prjjar

--- src/main/java/org/senai/prjjava/controller/AdminController.java
+++ src/main/java/org/senai/prjjava/controller/AdminController.java
@@ -36,16 +36,16 @@ public class AdminController {
         this.urepository = repository;
         this.encoder = encoder;
         this.pRepository = pRepository;
-        Usuario useradmin = new Usuario();
-        List<String> roles = new ArrayList<>();
-        roles.add("Admin");
-        useradmin.setRoles(roles);
-        useradmin.setLogin("admin");
-        useradmin.setEnabled(true);
-        useradmin.setPassword(encoder.encode("admin123"));
-        if (urepository.findByLogin("admin") == null){
-            urepository.save(useradmin);
-        }
+        // Usuario useradmin = new Usuario();
+        // List<String> roles = new ArrayList<>();
+        // roles.add("Admin");
+        // useradmin.setRoles(roles);
+        // useradmin.setLogin("admin");
+        // useradmin.setEnabled(true);
+        // useradmin.setPassword(encoder.encode("admin123"));
+        // if (urepository.findByLogin("admin") == null){
+        //     urepository.save(useradmin);
+        // }
     }
 
     @GetMapping("/listarusuarios")
